75a4144630cf2298a892bbefab79126ee42bcbb0
Julian Hirn <j.hirn@draftkings.com>
2021-12-01 23:03:05 -0500

    added day1

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,38 @@
+use crate::to_num_arr;
+
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    let data = to_num_arr(input);
+
+    let mut depth_incr_count = 0;
+    let mut prev_m = data[0];
+    for ii in 1..data.len() {
+        let m = data[ii];
+        if m > prev_m {
+            depth_incr_count += 1;
+        }
+
+        prev_m = m;
+    }
+
+    println!("{}", depth_incr_count)
+}
+
+pub fn part2(input: String) {
+    let data = to_num_arr(input);
+    assert!(data.len() >= 3);
+
+    let mut past_window = data[..=2].iter().sum::<i32>();
+    let mut curr_window = past_window;
+    let mut depth_incr_count = 0;
+
+    for ii in 3..data.len() {
+        curr_window = curr_window + data[ii] - data[ii-3];
+        if curr_window > past_window {
+            depth_incr_count += 1;
+        }
+
+        past_window = curr_window;
+    }
+
+    println!("{}", depth_incr_count);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,18 @@ pub type DayFn = fn(String);
 
 pub fn get_day(day: u32) -> (DayFn, DayFn) {
     return match day {
-        1 => (day01::part1, noop),
+        1 => (day01::part1, day01::part2),
         _ => {
             println!("Unknown day: {}", day);
             return (noop, noop);
         }
     };
+}
+
+pub fn to_num_arr(input: String) -> Vec<i32> {
+    input.split("\n").filter(|line| {
+        !line.is_empty()
+    }).map(|line| {
+        line.parse::<i32>().unwrap()
+    }).collect::<Vec<i32>>()
 }
