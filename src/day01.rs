178cdad07adb56afcc37ddf326aa374d1c7a81b4
Julian Hirn <j.hirn@draftkings.com>
2021-12-03 10:34:40 -0500

    improved day1

--- src/day01.rs
+++ src/day01.rs
@@ -1,19 +1,23 @@
 use crate::to_num_arr;
 
-pub fn part1(input: String) {
-    let data = to_num_arr(input);
-
-    let mut depth_incr_count = 0;
+fn increasing_count(data: Vec<i32>) -> i32 {
+    let mut incr_count: i32 = 0;
     let mut prev_m = data[0];
     for ii in 1..data.len() {
         let m = data[ii];
         if m > prev_m {
-            depth_incr_count += 1;
+            incr_count += 1;
         }
 
         prev_m = m;
     }
 
+    return incr_count;
+}
+
+pub fn part1(input: String) {
+    let data = to_num_arr(input);
+    let depth_incr_count = increasing_count(data);
     println!("{}", depth_incr_count)
 }
 
@@ -21,18 +25,16 @@ pub fn part2(input: String) {
     let data = to_num_arr(input);
     assert!(data.len() >= 3);
 
-    let mut past_window = data[..=2].iter().sum::<i32>();
-    let mut curr_window = past_window;
-    let mut depth_incr_count = 0;
+    let mut new_vec = Vec::<i32>::new();
+    let mut curr_window = data[0] + data[1] + data[2];
+    new_vec.push(curr_window);
 
+    // Skip first 2 indeces
     for ii in 3..data.len() {
         curr_window = curr_window + data[ii] - data[ii-3];
-        if curr_window > past_window {
-            depth_incr_count += 1;
-        }
-
-        past_window = curr_window;
+        new_vec.push(curr_window);
     }
 
+    let depth_incr_count = increasing_count(new_vec);
     println!("{}", depth_incr_count);
 }
