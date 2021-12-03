b74d47dd2f624b4b1bcded375d4cc0426785f982
Julian Hirn <adamai@pop-os.localdomain>
2021-12-03 18:22:25 -0500

    finished day03

--- src/day03.rs
+++ src/day03.rs
@@ -1,88 +1,122 @@
-use crate::bin_string_to_i32;
+use std::fmt;
 
-#[derive (Debug, Clone, Copy)]
-struct BinCount {
-    num_ones: u32,
-    num_zeroes: u32,
+// Unsigned boolean
+#[derive(Clone)]
+struct Bin {
+    data: Vec<bool>
 }
 
-fn get_bin_counts(data: Vec<&str>) -> Vec<BinCount> {
-    let mut counts = vec!(BinCount{num_ones: 0, num_zeroes: 0}; data[0].len());
+impl fmt::Debug for Bin {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Bin").field("data", &self.data).finish()
+    }
+}
 
-    for b in data {
-        for (ii, digit) in b.chars().enumerate() {
-            match digit {
-                '0' => counts[ii].num_zeroes += 1,
-                '1' => counts[ii].num_ones += 1,
-                _ => panic!("not valid bin"),
+impl Bin {
+    const BASE: usize = 2;
+
+    fn new() -> Bin {
+        Bin { data: Vec::new() }
+    }
+
+    fn from_binstr(binstr: &str) -> Bin {
+        Bin { data: binstr.chars().map(|x| if x == '1' { true } else { false }).collect() }
+    }
+
+    fn to_u32(&self) -> usize {
+        let mut result = 0;
+        for i in 0..self.data.len() {
+            if self.data[i] {
+                result += Bin::BASE.pow((self.data.len() - 1 - i) as u32);
             }
         }
+        result
     }
 
-    counts
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    fn push(&mut self, val: bool) {
+        self.data.push(val);
+    }
+
+    fn get(&self, index: usize) -> bool {
+        self.data[index]
+    }
 }
 
-fn get_most_least_common(bin_counts: Vec<BinCount>) -> (String, String) {
-    let mut most_common = String::new();
-    let mut least_common = String::new();
+fn most_common(bins: &Vec<Bin>, index: usize) -> bool {
+    let mut count = 0;
+    for bvec in bins {
+        if bvec.get(index) {
+            count += 1;
+        }
+    }
+    return 2 * count >= bins.len()
+}
 
-    for c in bin_counts {
-        if c.num_ones > c.num_zeroes {
-            most_common += "1";
-            least_common += "0";
+fn get_bins(input: String) -> Vec<Bin> {
+    input.split("\n").filter(|item| !item.is_empty()).map(Bin::from_binstr).collect::<Vec<_>>()
+}
+
+fn get_gamma_epsilon(bins: &Vec<Bin>) -> (Bin, Bin) {
+    let bin_len = bins[0].len();  // we assume that all the bins are the same length
+    let mut gbin = Bin::new();
+    let mut ebin = Bin::new();
+    for i in 0..bin_len {
+        if most_common(&bins, i) {
+            gbin.push(true);
+            ebin.push(false);
         } else {
-            most_common += "0";
-            least_common += "1";
+            gbin.push(false);
+            ebin.push(true);
         }
     }
 
-    (most_common, least_common)
+    (gbin, ebin)
 }
 
 pub fn part1(input: String) {
-    let bins = input.split("\n").collect::<Vec<_>>();
-    let counts = get_bin_counts(bins);
+    let bins = get_bins(input);
+    let (gbin, ebin) = get_gamma_epsilon(&bins);
+    let g = gbin.to_u32();
+    let e = ebin.to_u32();
 
-    let (most_common, least_common) = get_most_least_common(counts);
-    let gamma = bin_string_to_i32(&most_common);
-    let epsilon = bin_string_to_i32(&least_common);
-
-    println!("gamma {} epsilon {}, product {}", gamma, epsilon, gamma * epsilon);
-}
-
-fn prefix_match_length(s1: &str, s2: &str) -> u32 {
-    match s1.strip_prefix(s2) {
-        Some(a) => (s1.len() - a.len()) as u32,
-        None => 0,
-    }
-}
-
-// Finds the item in the haystack that shares the most number of characters with the needle
-fn longest_prefix_match(needle: String, haystack: Vec<&str>) -> Option<String> {
-    print!("{} {:?}\n", needle, haystack);
-    let mut longest_match = 0;
-    let mut longest_result: Option<String> = None;
-
-    for h in haystack {
-        let plen = prefix_match_length(&needle, &h);
-        if plen > longest_match {
-            println!("{} {} {}", h, plen, longest_match);
-            longest_match = plen;
-            longest_result = Some(h.to_string());
-        }
-    }
-
-    longest_result
+    println!("g {}, e {}, product {}", g, e, g * e);
 }
 
 pub fn part2(input: String) {
-    let bins = input.split("\n").collect::<Vec<_>>();
-    let counts = get_bin_counts(bins.clone());
+    let bins = get_bins(input);
+    let bin_len = bins[0].len();
 
-    let (most_common, least_common) = get_most_least_common(counts);
+    // find o2r
+    let mut candidates: Vec<Bin> = bins.to_vec();
+    for ii in 0..bin_len {
+        let (g, _) = get_gamma_epsilon(&candidates);
+        let expected = g.get(ii);
+        candidates = candidates.into_iter().filter(|b| b.get(ii) == expected).collect();
 
-    let oxygen = longest_prefix_match(most_common, bins.clone()).unwrap();
-    let co2 = longest_prefix_match(least_common, bins.clone()).unwrap();
-    // get longest prefix match
-    println!("{} {}", oxygen, co2);
+        if candidates.len() == 1 {
+            break;
+        }
+    }
+    assert!(candidates.len() == 1);
+    let o2r = &candidates[0].to_u32();
+
+    // find co2r
+    let mut candidates: Vec<Bin> = bins.to_vec();
+    for ii in 0..bin_len {
+        let (_, e) = get_gamma_epsilon(&candidates);
+        let expected = e.get(ii);
+        candidates = candidates.into_iter().filter(|b| b.get(ii) == expected).collect();
+
+        if candidates.len() == 1 {
+            break;
+        }
+    }
+    assert!(candidates.len() == 1);
+    let co2r = &candidates[0].to_u32();
+
+    println!("o2 rating {}, co2 rating {}, product {}", o2r, co2r, o2r * co2r);
 }
